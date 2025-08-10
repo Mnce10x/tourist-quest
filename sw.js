2a6620055ae0935f72893e9c27bcdcc27a642245
Mnce10x <mncedisi.mtshali@outlook.com>
2025-08-10 14:09:28 +0200

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,48 @@
+// Minimal offline-first service worker for Tourist Quest 2D
+const CACHE = 'tq2d-v1';
+const PRECACHE = [
+  './',
+  './index.html',
+  './manifest.webmanifest',
+  './icons/icon-192.png',
+  './icons/icon-512.png',
+  'https://unpkg.com/react@18/umd/react.production.min.js',
+  'https://unpkg.com/react-dom@18/umd/react-dom.production.min.js',
+  'https://unpkg.com/@babel/standalone/babel.min.js',
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE).then((cache) => cache.addAll(PRECACHE)).then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) => Promise.all(keys.map((k) => (k === CACHE ? null : caches.delete(k))))).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  const req = event.request;
+  const url = new URL(req.url);
+  // Network-first for navigation requests (HTML), cache-first otherwise.
+  if (req.mode === 'navigate') {
+    event.respondWith(
+      fetch(req).then((res) => {
+        const copy = res.clone();
+        caches.open(CACHE).then((cache) => cache.put(req, copy));
+        return res;
+      }).catch(() => caches.match(req).then((res) => res || caches.match('./index.html')))
+    );
+  } else {
+    event.respondWith(
+      caches.match(req).then((res) => res || fetch(req).then((net) => {
+        // cache a copy
+        const copy = net.clone();
+        caches.open(CACHE).then((cache) => cache.put(req, copy));
+        return net;
+      }).catch(() => res))
+    );
+  }
+});
